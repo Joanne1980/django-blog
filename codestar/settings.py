3f7322e880a8cb4427990dd936f07d562da9e31a
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-11-12 14:51:16 +0000

    Add: post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ['8000-joanne1980-djangoblog-8zabr6vzwig.ws.codeinstitute-ide.net', 'blog-walkthrough-5715b26add6a.herokuapp.com']
-# CSRF_TRUSTED_ORIGINS =['https://*.codeinstitute-ide.net','https://*.herokuapp.com']
+CSRF_TRUSTED_ORIGINS =['https://*.codeinstitute-ide.net',
+'https://*.herokuapp.com']
 
 # Application definition
 
