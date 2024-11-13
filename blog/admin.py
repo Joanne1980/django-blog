d0b9c7dbd77b03fcff3956ef0af1a8d6ec3b1220
Sarah Joanne <joanne.clarke23@hotmail.com>
2024-11-13 15:17:21 +0000

    Add new migrations for comment table in database

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Post, comments
+from .models import Post, Comment
 
 # Register your models here.
 admin.site.register(Post)
-admin.site.register(comments)
+admin.site.register(Comment)
